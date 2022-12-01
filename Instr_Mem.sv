c51fd690c38f3c92d27a1017470fc0dc7809e6b3
shaopyshaozi <shaopengyuanspy@outlook.com>
2022-12-01 16:39:29 +0000

    Create Instr_Mem.sv
    New instruction memory has address width 12 bits (lower 12 bits of the instruction). Now the instruction starts from BFC000000 to BFC00FFF. We have to change the code in counter so that it counts from BFC00000 as required.

---
+++ Instr_Mem.sv
@@ -0,0 +1,21 @@
+module Instr_Mem # (
+    parameter   ADDRESS_WIDTH = 32,
+                DATA_WIDTH = 8,
+                OUT_WIDTH = 32
+)(
+    input logic [ADDRESS_WIDTH-1:0] A,
+    output logic [OUT_WIDTH-1:0] RD
+);
+
+logic [11:0] addr = A[11:0] 
+
+logic [DATA_WIDTH-1:0] array [2**12:0];
+
+initial begin
+        $display ("Loading Instr_Mem.");
+        $readmemh("counter.mem", array); 
+end;
+
+assign RD = {array [addr+3], array [addr+2], array [addr+1], array[addr+0]};
+
+endmodule
