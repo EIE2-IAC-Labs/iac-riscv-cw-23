30196c2bda3230fb1b0f7af02b7e31fff765e779
shaopyshaozi <shaopengyuanspy@outlook.com>
2022-12-03 18:12:59 +0000

    Update Sign_extend.sv
    add extend for instruction load upper immediate. {instr[31:12], 12'b0}

--- Sign_extend.sv
+++ Sign_extend.sv
@@ -1,31 +1,36 @@
 module Sign_extend (
-    input logic [1:0]   ImmSrc,
+    input logic [2:0]   ImmSrc,
     input logic [31:0]  instr,
     output logic [31:0] ImmOp
 );
 
+    logic dummy_wire;
+
 always_comb
 
     casez(ImmSrc)
-    2'b00:  if (instr[31])                                      // I type   12 bits immediate is from the instruction bit [31:20]
+    default: assign dummy_wire = 0; 
+    3'b000: if (instr[31])                                      // I type   12 bits immediate is from the instruction bit [31:20]
                 assign ImmOp = ({20'hFFFFF, instr[31:20]});
             else
                 assign ImmOp = ({20'b0, instr[31:20]});
 
-    2'b01:  if (instr[31])                                      // S type 
+    3'b001: if (instr[31])                                      // S type 
                 assign ImmOp = ({20'hFFFFF, instr[31:25], instr[11:7]});
             else
                 assign ImmOp = ({20'b0, instr[31:25], instr[11:7]});
 
-    2'b10:  if (instr[31])                                      // B type
+    3'b010: if (instr[31])                                      // B type
                 assign ImmOp = ({20'hFFFFF, instr[31], instr[7], instr[30:25], instr[11:8]});
             else
                 assign ImmOp = ({20'b0,  instr[31], instr[7], instr[30:25], instr[11:8]});
     
-    2'b11:  if (instr[31])                                      // J type
+    3'b011: if (instr[31])                                      // J type
                 assign ImmOp = ({20'hFFFFF, instr[31], instr[19:12], instr[20], instr[30:21]});
             else
                 assign ImmOp = ({20'b0, instr[31], instr[19:12], instr[20], instr[30:21]});
+
+    3'b100: assign ImmOp = ({instr[31:12], 12'b0});            // U type
 
     endcase
     
