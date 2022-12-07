5123460ba09d1ef1623c705ca4848907e1a32de1
Karry939 <116262630+Karry939@users.noreply.github.com>
2022-12-07 15:31:51 +0000

    Zero

--- rtl/ALU.sv
+++ rtl/ALU.sv
@@ -2,7 +2,7 @@ module ALU #(
     parameter DATA_WIDTH = 32
               //CONTROL_SIGNAL = 1
 ) (
-    input logic ALUctrl,
+    input logic [2:0] ALUctrl,
     input logic [DATA_WIDTH-1:0] ALUop1,
     input logic [DATA_WIDTH-1:0] ALUop2,
     output logic [DATA_WIDTH-1:0] ALUResult,
@@ -21,10 +21,12 @@ module ALU #(
 
     always_comb 
         casez(ALUctrl)
-        1'b1: assign SUM = ALUop1 + ALUop2;
-        1'b0: if (ALUop1 - ALUop2 == {DATA_WIDTH{1'b0}}) assign EQ = 1'b1; 
+        3'b000: assign ALUResult = ALUop1 + ALUop2;
+        3'b001: assign ALUResult = ALUop1 - ALUop2;
+         if (ALUResult == {DATA_WIDTH{1'b0}}) assign Zero = 1'b1; 
               else assign Zero = 1'b0;
         default: Zero = 1'b0;
+        3'b110: assign ALUResult = ALUop2;
         endcase
 endmodule
 
