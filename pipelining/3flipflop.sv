44d3679688a8c4d0c508b93eba479b2e7c738ef2
anish-narain <69715492+anish-narain@users.noreply.github.com>
2022-12-08 22:27:40 +0000

    flip flop 3 changes

--- pipelining/3flipflop.sv
+++ pipelining/3flipflop.sv
@@ -1,7 +1,40 @@
 module 3flipflop(
-    input logic         clk;
-    input logic         ALUOut;
-    input logic         
+    input logic         clk,
+    //remaining inputs
+    input logic         ALUOut,
+    input logic         regOp2, //writeDataE in diagram
+    input logic         RdE,
+    input logic         PCTargetE,
+    input logic         PCPlus4E,
+    //control signal inputs
+    input logic         RegWriteE,
+    input logic         ResultSrcE,
+    input logic         MemWriteE,
+    input logic         MUXJUMPE,
+    input logic         JUMPRTE,
+    //control signal outputs
+    output logic        RegWriteM,
+    output logic        ResultSrcM,
+    output logic        MemWriteM,
+    output logic        MUXJUMPM,
+    output logic        JUMPRTM,
+    //remaining outputs
+    output logic        ALUResultM,
+    output logic        WriteDataM,
+    output logic        RdM,
+    output logic        PCTargetM,
+    output logic        PCPlus4M
 );
 
+always_ff @(posedge clk)
+    begin
+        RegWriteM <= RegWriteE;
+        ResultSrcM <= ResultSrcE;
+        MemWriteM <= MemWriteE;
+        ALUResultM <= ALUOut;
+        WriteDataM <= regOp2;
+        RdM <= RdE;
+        PCTargetM <= PCTargetE;
+        PCPlus4M <= PCPlus4E;
+    end
 endmodule
---
+++ pipelining/4flipflop.sv
@@ -0,0 +1,5 @@
+module 4flipflop (
+    input logic         
+);
+
+endmodule
