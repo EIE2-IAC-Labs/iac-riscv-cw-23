15c162df7cdeaf48b5e6a3ba90370059ba0aba84
anish-narain <69715492+anish-narain@users.noreply.github.com>
2022-12-11 17:34:32 +0000

    data memory changes for byte addressing

--- rtl3/DataMemory.sv
+++ rtl3/DataMemory.sv
@@ -1,9 +1,10 @@
 module DataMemory #(
     parameter ADDRESS_WIDTH = 32,
-              DATA_WIDTH = 32
+              DATA_WIDTH = 8
 )(
     input logic             clk,
     input logic             WE,
+    input logic             addr_mode,
     input logic [ADDRESS_WIDTH-1:0] A,
     input logic [DATA_WIDTH-1:0] WD,
     output logic [DATA_WIDTH-1:0] RD
