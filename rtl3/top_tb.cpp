5e6faffbb690fb134a9edbfb93739e5f78f82c3d
anish-narain <69715492+anish-narain@users.noreply.github.com>
2022-12-12 15:00:38 +0000

    retrying test for reference program

--- rtl3/top_tb.cpp
+++ rtl3/top_tb.cpp
@@ -3,7 +3,7 @@
 #include "Vtop.h"
 
 #include "vbuddy.cpp"     // include vbuddy code
-#define MAX_SIM_CYC 200
+#define MAX_SIM_CYC 300000
 
 int main(int argc, char **argv, char **env) {
   int simcyc;     // simulation clock count
@@ -40,17 +40,9 @@ int main(int argc, char **argv, char **env) {
     top->TRIGGERSEL = 1;
     
     // plot ROM output and print cycle count
-    vbdHex(4, (int(top->a0) >> 16) & 0xF);
-    vbdHex(3, (int(top->a0) >> 8) & 0xF);
-    vbdHex(2, (int(top->a0) >> 4) & 0xF);
-    vbdHex(1, int(top->a0) & 0xF);
+    vbdPlot(int(top->a0), 0, 255);
     vbdCycle(simcyc+1);
 
-        // Display toggle neopixel
-   
-    vbdBar(top->a0);
-    top->rst = vbdFlag();
-    
     // either simulation finished, or 'q' is pressed
     if ((Verilated::gotFinish()) || (vbdGetkey()=='q')) 
       exit(0);                // ... exit if finish OR 'q' pressed
